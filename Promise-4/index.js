a3f476aa5408914d8dd26c091979392dd651b7b0
xiluna <xilaluna2@gmail.com>
2022-04-05 14:56:45 -0700

    [update] challenge 4 done

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -1,14 +1,15 @@
 const food = new Promise((resolve, reject) => {
-  
+  setTimeout(() => {
+    resolve("Jerk Chicken!!")
+  }, 2000)
 })
 
+// resolve food here:
 
-// resolve food here: 
+// **Challenge 1** Resolve your promise with the name of your favorite food.
+// Do this by calling the `resolve` with an argument.
 
+// **Challenge 2** Resolve the `food` promise with that `.then()` syntax
+// and print your favorite food to the console.
 
-
-// **Challenge 1** Resolve your promise with the name of your favorite food. 
-// Do this by calling the `resolve` with an argument. 
-
-// **Challenge 2** Resolve the `food` promise with that `.then()` syntax 
-// and print your favorite food to the console. 
+food.then((food) => console.log(food))
