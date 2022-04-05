5abe95ca967ca3c61c1de6d442cef2bde826f2cc
xiluna <xilaluna2@gmail.com>
2022-04-05 15:41:34 -0700

    [update] challenge 7 done

--- Promise-7/script.js
+++ Promise-7/script.js
@@ -1,43 +1,46 @@
-
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
-  maximumAge: 0
-};
-
-function onsuccess(pos) {
-  const { latitude, longitude, accuracy } = pos.coords;
-  console.log('Your current position is:');
-  console.log(`Latitude : ${latitude}`);
-  console.log(`Longitude: ${longitude}`);
-  console.log(`More or less ${accuracy} meters.`);
-} 
-
-function onerror(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
+  maximumAge: 0,
 }
 
-navigator.geolocation.getCurrentPosition(onsuccess, onerror, options);
+function onsuccess(pos) {
+  const { latitude, longitude, accuracy } = pos.coords
+  console.log("Your current position is:")
+  console.log(`Latitude : ${latitude}`)
+  console.log(`Longitude: ${longitude}`)
+  console.log(`More or less ${accuracy} meters.`)
+}
+
+function onerror(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`)
+}
+
+navigator.geolocation.getCurrentPosition(onsuccess, onerror, options)
 // navigator.geolocation( successCallback, errorCallback, options )
 
 // navigator.geolocation((pos) => {
-//   // 
+//   //
 // }, (err) => {
-//   // 
+//   //
 // }, {})
 
-
-// Challenge: Make this work: 
+// Challenge: Make this work:
 
 function getGeolocation(options) {
-  return new Promise( (resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options);
-  } )
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options)
+  })
 }
 
-// Like this: 
-
 getGeolocation(options)
-  .then(pos => { })
-  .catch(err => { })
-
+  .then((pos) => {
+    const { latitude, longitude, accuracy } = pos.coords
+    console.log("Your current position is:")
+    console.log(`Latitude : ${latitude}`)
+    console.log(`Longitude: ${longitude}`)
+    console.log(`More or less ${accuracy} meters.`)
+  })
+  .catch((err) => {
+    console.warn(`ERROR(${err.code}): ${err.message}`)
+  })
