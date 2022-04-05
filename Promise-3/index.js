8b305f04ba813e9902a3193ec4309e7a52e377c6
xiluna <xilaluna2@gmail.com>
2022-04-05 14:41:03 -0700

    [update] challenge 3 done

--- Promise-3/index.js
+++ Promise-3/index.js
@@ -1,43 +1,51 @@
 function greet(name) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof name === 'string') { 
-        resolve('hello ' + name);
+      if (typeof name === "string") {
+        resolve("hello " + name)
       } else {
-        reject('Name must be a string!');
+        reject("Name must be a string!")
       }
-    }, 1000);
-  });
+    }, 1000)
+  })
 }
 
 function uppercaser(str) {
   return new Promise((resolve, reject) => {
-    setTimeout(() =>{
-      if (typeof str === 'string') {
-        resolve(str.toUpperCase());
+    setTimeout(() => {
+      if (typeof str === "string") {
+        resolve(str.toUpperCase())
       } else {
-        reject('Argument to uppercaser must be string');
+        reject("Argument to uppercaser must be string")
       }
-    }, 1000);
-  });
+    }, 1000)
+  })
 }
 
+// Above we have two functions that returns promises.
+// These are greet and uppercaser.
+// Notice how we chain these promises together. The
+// Result of one is then passed to the next.
 
-// Above we have two functions that returns promises. 
-// These are greet and uppercaser. 
-// Notice how we chain these promises together. The 
-// Result of one is then passed to the next. 
-
-greet('Your name') // Returns a Promise
-.then(str => uppercaser(str))  // Upper case the results from greet() Returns a Promise
-.then(str => console.log(str)) // Log the results of uppercaser()
-.catch(err => console.log(err)) // Catches an error
+greet("Xila") // Returns a Promise
+  .then((str) => uppercaser(str)) // Upper case the results from greet() Returns a Promise
+  .then((str) => console.log(str)) // Log the results of uppercaser()
+  .catch((err) => console.log(err)) // Catches an error
 
 // Challenges: get greet() to fail by passing a non string value
-// What happens? 
+// What happens?
+
+greet(2)
+  .then((str) => console.log(str))
+  .catch((err) => console.log(err))
 
 // Challenge: get uppercaser() to fail by passing a non string value
-// What happens? 
+// What happens?
 
-// Challenge: Notice there is only a single .catch() at the end. 
+uppercaser(5).catch((error) => console.log(error))
+
+// Challenge: Notice there is only a single .catch() at the end.
 // Explain the behavior?
+
+// There is only a single catch at the end because it catches for both of the functions becuase there chained together.
+// also there is one input
