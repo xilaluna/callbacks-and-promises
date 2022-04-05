97a5d9c7417d5557f305ef5828d2899c3843bc70
xiluna <xilaluna2@gmail.com>
2022-04-05 14:24:41 -0700

    [update] challenge 2 done

--- Promise-2/index.js
+++ Promise-2/index.js
@@ -2,19 +2,34 @@ function asyncNumberChecker(n) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (n > 9) {
-        reject('Only numbers less than 10 please!')
+        reject("Only numbers less than 10 please!")
       } else {
-        resolve('I can work with this!')
+        resolve("I can work with this!")
       }
-    }, 3333);
-  });
+    }, 3333)
+  })
 }
 
-// The function above returns a promise. Your job is 
-// to handle the promise in the function calls below. 
+// The function above returns a promise. Your job is
+// to handle the promise in the function calls below.
 // Handle success with .then() and errors with .catch()
 
 asyncNumberChecker(10)
-
+  .then((message) => {
+    console.log("Number is less than 10 ✅")
+    console.log(message)
+  })
+  .catch((err) => {
+    console.log("Number is not less than 10 ❌")
+    console.log(err)
+  })
 
 asyncNumberChecker(5)
+  .then((message) => {
+    console.log("Number is less than 10 ✅")
+    console.log(message)
+  })
+  .catch((err) => {
+    console.log("Number is not less than 10 ❌")
+    console.log(err)
+  })
